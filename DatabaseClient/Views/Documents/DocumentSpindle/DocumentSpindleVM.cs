3d22728417b8d7b248134927035dbed458affe21
Marcin Borysiak <marcinez24@gmail.com>
2019-07-22 09:15:42 +0200

    Added View for spindleDocuments

---
+++ DatabaseClient/Views/Documents/DocumentSpindle/DocumentSpindleVM.cs
@@ -0,0 +1,26 @@
+﻿using DatabaseClient.EntityData;
+using Support;
+
+namespace DatabaseClient
+{
+    public class DocumentSpindleVM : VMBase
+    {
+        public document_spindle TheEntity
+        {
+            get
+            {
+                return (document_spindle)base.theEntity;
+            }
+            set
+            {
+                theEntity = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DocumentSpindleVM()
+        {
+            TheEntity = new document_spindle();
+        }
+    }
+}
