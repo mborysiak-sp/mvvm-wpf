5b3d70572b59e4355ab3ccf23e4b12319800f583
Marcin Borysiak <marcinez24@gmail.com>
2019-07-25 10:34:52 +0200

    Redone project GUI to Material Design

--- DatabaseClient/Views/Main/MainWindowViewModel.cs
+++ DatabaseClient/Views/Main/MainWindowViewModel.cs
@@ -17,6 +17,7 @@ namespace DatabaseClient
         {
             ObservableCollection<ViewVM> views = new ObservableCollection<ViewVM>
             {
+<<<<<<< Updated upstream
                 new ViewVM { ViewDisplay="Spindles", ViewType = typeof(SpindlesView), ViewModelType = typeof(SpindlesViewModel)},
                 new ViewVM { ViewDisplay="BoringBars", ViewType = typeof(BoringBarsView), ViewModelType = typeof(BoringBarsViewModel)},
                 new ViewVM { ViewDisplay="Bearings", ViewType = typeof(BearingsView), ViewModelType = typeof(BearingsViewModel)},
@@ -25,6 +26,16 @@ namespace DatabaseClient
                 new ViewVM { ViewDisplay="AllDocuments", ViewType = typeof(AllDocumentsView), ViewModelType = typeof(AllDocumentsViewModel)},
                 new ViewVM { ViewDisplay="DocumentBoringBar", ViewType = typeof(DocumentBoringBarView), ViewModelType = typeof(DocumentBoringBarViewModel)},
                 new ViewVM { ViewDisplay="DocumentSpindle", ViewType = typeof(DocumentSpindleView), ViewModelType = typeof(DocumentSpindleViewModel)}
+=======
+                new ViewVM { ViewDisplay="Wrzeciona", ViewType = typeof(SpindlesView), ViewModelType = typeof(SpindlesViewModel)},
+                new ViewVM { ViewDisplay="Wytaczadła", ViewType = typeof(BoringBarsView), ViewModelType = typeof(BoringBarsViewModel)},
+                new ViewVM { ViewDisplay="Łożyska", ViewType = typeof(BearingsView), ViewModelType = typeof(BearingsViewModel)},
+                new ViewVM { ViewDisplay="Wrzeciona złom.", ViewType = typeof(ScrappedSpindlesView), ViewModelType = typeof(ScrappedSpindlesViewModel)},
+                new ViewVM { ViewDisplay="Wytaczadła złom.", ViewType = typeof(ScrappedBoringBarsView), ViewModelType = typeof(ScrappedBoringBarsViewModel)},
+                new ViewVM { ViewDisplay="Świadectwa", ViewType = typeof(AllDocumentsView), ViewModelType = typeof(AllDocumentsViewModel)},
+                new ViewVM { ViewDisplay="Świadectwa wytaczadeł", ViewType = typeof(DocumentBoringBarView), ViewModelType = typeof(DocumentBoringBarViewModel)},
+                new ViewVM { ViewDisplay="Świadectwa wrzecion", ViewType = typeof(DocumentSpindleView), ViewModelType = typeof(DocumentSpindleViewModel)}
+>>>>>>> Stashed changes
             };
             Views = views;
             RaisePropertyChanged("Views");
@@ -32,10 +43,17 @@ namespace DatabaseClient
 
             ObservableCollection<CommandVM> commands = new ObservableCollection<CommandVM>
             {
+<<<<<<< Updated upstream
                 new CommandVM{ CommandDisplay="Insert", IconGeometry=Application.Current.Resources["InsertIcon"] as Geometry , Message=new CommandMessage{ Command =CommandType.Insert}},
                 new CommandVM{ CommandDisplay="Edit", IconGeometry=Application.Current.Resources["EditIcon"] as Geometry , Message=new CommandMessage{ Command = CommandType.Edit}},
                 new CommandVM{ CommandDisplay="Delete", IconGeometry=Application.Current.Resources["DeleteIcon"] as Geometry , Message=new CommandMessage{ Command = CommandType.Delete}},
                 new CommandVM{ CommandDisplay="Refresh", IconGeometry=Application.Current.Resources["RefreshIcon"] as Geometry , Message=new CommandMessage{ Command = CommandType.Refresh}}
+=======
+                new CommandVM{ CommandDisplay="Dodaj", IconGeometry=Application.Current.Resources["InsertIcon"] as Geometry , Message=new CommandMessage{ Command =CommandType.Insert}},
+                new CommandVM{ CommandDisplay="Edytuj", IconGeometry=Application.Current.Resources["EditIcon"] as Geometry , Message=new CommandMessage{ Command = CommandType.Edit}},
+                new CommandVM{ CommandDisplay="Usuń", IconGeometry=Application.Current.Resources["DeleteIcon"] as Geometry , Message=new CommandMessage{ Command = CommandType.Delete}},
+                new CommandVM{ CommandDisplay="Odśwież", IconGeometry=Application.Current.Resources["RefreshIcon"] as Geometry , Message=new CommandMessage{ Command = CommandType.Refresh}}
+>>>>>>> Stashed changes
             };
             Commands = commands;
             RaisePropertyChanged("Commands");
