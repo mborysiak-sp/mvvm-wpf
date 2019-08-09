53af6f023b85b9955c0ebec42171313d5375278b
Marcin Borysiak <marcinez24@gmail.com>
2019-08-09 14:00:35 +0200

    Last commit probably

--- DatabaseClient/EntityData/boring_bar.cs
+++ DatabaseClient/EntityData/boring_bar.cs
@@ -12,7 +12,7 @@ namespace DatabaseClient.EntityData
     using System;
     using System.Collections.Generic;
     
-    public partial class boring_bar : IEntityWithId
+    public partial class boring_bar
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public boring_bar()
